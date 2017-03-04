1a3f943fc85cd86ba93714f23269178e16f85f5e
hht <hht>
2017-03-04 16:27:29 +0800

    init

---
+++ app/js/index.js
@@ -0,0 +1,16 @@
+$(function() {
+	var mySwiper1 = new Swiper('.swiper-container1',{
+		prevButton:'.swiper-button-prev',
+		nextButton:'.swiper-button-next',
+		pagination :'.swiper-pagination',
+		autoplay:5000,
+		loop:true
+	});
+	var mySwiper2 = new Swiper('.swiper-container2',{
+		prevButton:'.swiper-button-prev',
+		nextButton:'.swiper-button-next',
+		pagination :'.swiper-pagination',
+		autoplay:5000,
+		loop:true
+	})
+});
